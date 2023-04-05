d3e6f4089bac20eca8f2737129716043e4ed5b75
Luiz Henrique de Oliveira <luizhsimionato@gmail.com>
2023-04-05 16:20:09 -0300

    contador

---
+++ src/components/contador/index.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import Botao from '../botao'
+
+class Contador extends Component{
+    
+    constructor(props){
+       super(props)
+       this.state= {
+           contador: 0,
+       }
+       this.aumentar = this.aumentar.bind(this)
+       this.diminuir = this.diminuir.bind(this)
+    }
+ 
+
+     diminuir(){
+        let state = this.state
+        if(this.state.contador ===0){
+            return
+        }
+        state.contador -=1
+        this.setState(state)
+     }
+
+     aumentar(){
+        let state = this.state
+        if(this.state.contador ===20){
+            return
+        }
+        state.contador +=1
+        this.setState(state)
+     }
+    
+        render(){
+            return(
+                <div>
+                <h2>Hora certa: {this.state.hora}
+                </h2>
+                <h2>Contador Positivo</h2>
+                <Botao nome='Diminuir' acaoBtn={this.diminuir}/>
+                <h2>{this.state.contador}</h2><br />
+                <Botao nome='Aumentar' acaoBtn={this.aumentar}/>
+            </div>
+            );
+        }
+    } 
+export default Contador
